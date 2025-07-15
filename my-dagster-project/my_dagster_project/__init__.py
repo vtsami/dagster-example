cf158c5cab78fdcfb39c4811b96c5a995bc936aa
vtsami <vtsami@teza.com>
2025-07-15 13:51:06 +0100

    Update __init__.py

--- my-dagster-project/my_dagster_project/__init__.py
+++ my-dagster-project/my_dagster_project/__init__.py
@@ -18,3 +18,21 @@ defs = Definitions(
     ],
     resources={"github_api": Github(os.environ["GITHUB_ACCESS_TOKEN"])},
 )
+
+from dagster import (
+    load_assets_from_modules,
+    Definitions,
+    define_asset_job,
+    ScheduleDefinition,
+)
+from my_dagster_project import assets
+
+defs = Definitions(
+    assets=load_assets_from_modules([assets]),
+    schedules=[
+        ScheduleDefinition(
+            job=define_asset_job(name="daily_refresh", selection="*"),
+            cron_schedule="@daily",
+        )
+    ],
+)
