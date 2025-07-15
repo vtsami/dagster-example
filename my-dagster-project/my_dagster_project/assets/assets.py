28a59ef7fbf651e091ed1076d170bc67a1c011d2
vtsami <vtsami@teza.com>
2025-07-15 13:49:03 +0100

    Update assets.py

--- my-dagster-project/my_dagster_project/assets/assets.py
+++ my-dagster-project/my_dagster_project/assets/assets.py
@@ -24,3 +24,26 @@ def github_stargazers_by_week(github_stargazers):
         ]
     )
     return df.groupby("week").count().sort_values(by="week")
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import pickle
+import jupytext
+
+@asset
+def github_stars_notebook(github_stargazers_by_week):
+    markdown = f"""
+### Github Stars
+
+```python
+import pickle
+github_stargazers_by_week = pickle.loads({pickle.dumps(github_stargazers_by_week)!r})
+
+#### Github Stars by Week, last 52 weeks
+
+github_stargazers_by_week.tail(52).reset_index().plot.bar(x="week", y="users")
+
+    """
+    nb = jupytext.reads(markdown, "md")
+    ExecutePreprocessor().preprocess(nb)
+    return nbformat.writes(nb)
